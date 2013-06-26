8e50533a04ad1ec3b3e9b23ca9e692aae981f169
Felipe Coury <felipe.coury@gmail.com>
2013-06-26 13:19:11 -0300

    refactored config read

--- api.go
+++ api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "path/filepath"
   "fmt"
   // "io"
   "io/ioutil"
@@ -10,27 +11,39 @@ import (
   "strings"
 )
 
-func GetCredentials() {
+func getHomePath(file string) string {
   usr, err := user.Current()
   if err != nil {
     log.Fatal(err)
   }
-  b, err := ioutil.ReadFile(usr.HomeDir + "/.webbynode")
+  return filepath.Join(usr.HomeDir, file)
+}
+
+func readConfig() []string {
+  b, err := ioutil.ReadFile(getHomePath(".webbynode"))
   if err != nil {
     panic(err)
   }
-  lines := strings.Split(string(b), "\n")
-  config := &WebbynodeConfig{}
-  for _, line := range lines {
+  return strings.Split(string(b), "\n")
+}
+
+func GetCredentials() {
+  config := WebbynodeConfig{}
+  config.Load()
+  fmt.Println(config)
+}
+
+func (cfg *WebbynodeConfig) Load() error {
+  for _, line := range readConfig() {
     if strings.TrimSpace(line) != "" {
       parts := strings.SplitN(line, "=", 2)
-      fmt.Println(parts[0])
+      fmt.Println(parts)
       if parts[0] == "email" {
-        config.Email = parts[1]
+        cfg.Email = parts[1]
       }
     }
   }
-  fmt.Println(config)
+  return nil
 }
 
 type WebbynodeConfig struct {
